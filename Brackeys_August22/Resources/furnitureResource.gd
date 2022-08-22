a8f63b1c30cfb3c071292e67b055026a140c1e1c
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2022-08-22 20:30:32 +0530

    Added Window Lighting 2D

--- Brackeys_August22/Resources/furnitureResource.gd
+++ Brackeys_August22/Resources/furnitureResource.gd
@@ -6,4 +6,8 @@ export (Image) var furnitureSprite;
 export (Vector2) var spawnPosition;
 export (Vector2) var furnitureSize;
 
+export (String) var furnitureType;
+export (float) var lightIntensity;
+export (float) var lightRange;
+
 export (Array, String) var dialogSet; 
--- Brackeys_August22/scripts/Interactables/furnitures.gd
+++ Brackeys_August22/scripts/Interactables/furnitures.gd
@@ -1,9 +1,20 @@
 extends Node2D
 
+onready var lightTexture = load("res://assets/Items/lightTexture.png")
 
 func setSprite(selfData):
 	$Sprite.scale = selfData.furnitureSize;
 	if(selfData.furnitureSprite != null):
 	  $Sprite.texture =  selfData.furnitureSprite
+	activateLightSource(selfData)
+	
+func activateLightSource(selfData):
+	if(selfData.furnitureType == "light"):
+		var lightNode = Light2D.new()
+		lightNode.texture = lightTexture;
+		lightNode.energy = selfData.lightIntensity;
+		lightNode.scale = Vector2(selfData.lightRange, selfData.lightRange)
+		add_child(lightNode)
+		
 	
 
