de81a2b22c304698aacf29f09388401c8e3b6fd6
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2022-08-22 21:44:45 +0530

    Added ItemSlot Section

--- Brackeys_August22/scripts/Environment/World.gd
+++ Brackeys_August22/scripts/Environment/World.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 onready var generalRoomScene = load("res://scenes/Rooms/GeneralRoom.tscn")
-
+onready var itemSlots = $itemSlots;
 
 
 func _ready():
 	var room = generalRoomScene.instance();
-	add_child(room)
+	$spawnLayer.add_child(room)
 	room.setRoom("attic")
 	
 
--- Brackeys_August22/scripts/Player/PlayerMain.gd
+++ Brackeys_August22/scripts/Player/PlayerMain.gd
@@ -1,17 +1,23 @@
  extends KinematicBody2D
 
 
+onready var worldNode = get_tree().root.get_child(0)
+onready var itemSlots : Node2D;
+onready var dialogBox_scene = load("res://scenes/Interactables/dialogBox.tscn")
+
+
 onready var playerData = load("res://Store/playerData.tres")
 onready var player = get_node(".");
 onready var playerSprite = $Sprite;
 
-onready var dialogBox_scene = load("res://scenes/Interactables/dialogBox.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player.position = playerData.spawnPoint;
-	#dialogTest("Hello World")
+	itemSlots = worldNode.itemSlots;
+	
+	
 	
 func dialogTest(textToLoad: String):
 	var dialogBox = dialogBox_scene.instance();
